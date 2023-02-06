9bd0363d5a072aa6500252da3807aa8ac59679ee
codegram <omondihills02@gmail.com>
2023-02-06 21:19:54 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("message");
-document.write("messgae")
+
+let watch=document.getElementById("time");
+let time=new Date();
+console.log(time.getHours());
+console.log(time.getMinutes());
+
+function display(){
+    watch.style.backgroundColor="red";
+    watch.innerHTML=`${time.getHours()}: ${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`;
+}
+
+setInterval(display, 1);
