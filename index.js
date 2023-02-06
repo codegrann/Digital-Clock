d744fd0dc3b16234ab9993b427dd5aba0a559c45
codegram <omondihills02@gmail.com>
2023-02-06 21:28:44 +0300

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 
-let watch=document.getElementById("time");
+let watch=document.querySelector("#time");
 let time=new Date();
-console.log(time.getHours());
-console.log(time.getMinutes());
+// console.log(time.getHours());
+// console.log(time.getMinutes());
+
+
 
 function display(){
-    watch.style.backgroundColor="red";
-    watch.innerHTML=`${time.getHours()}: ${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`;
+    // watch.style.backgroundColor="red";
+    watch.innerText=`${time.getHours()}: ${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`;
+    // watch.innerText=`${time}`;
 }
 
-setInterval(display, 1);
+setInterval(display, 1000);
