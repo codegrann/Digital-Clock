c0cf95dea9ed99fda3a595b99e7b2aea9463fdc0
codegram <omondihills02@gmail.com>
2023-02-07 12:21:39 +0300

    added secHand to position itself according to current seconds

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 
 let watch=document.querySelector("#time");
-let time=new Date();
+
+
 // console.log(time.getHours());
 // console.log(time.getMinutes());
 
 
 
 function display(){
-    // watch.style.backgroundColor="red";
-    watch.innerText=`${time.getHours()}: ${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}`;
+    let time=new Date();
+    watch.innerText=`${time.getHours()}: ${time.getMinutes()}:${time.getSeconds()}`;
     // watch.innerText=`${time}`;
 }
-
 setInterval(display, 1000);
+
+function rotate(){
+    let secHand=document.querySelector("#secHand");
+    let time=new Date();
+    let sec=time.getSeconds();
+    console.log(sec);
+    secDegree=((sec/60)*360) + 90;
+    // secHand.style.height="10px";
+    secHand.style.transform=`rotate(${secDegree}deg)`;
+}
+setInterval(rotate, 1000);
